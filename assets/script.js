9a2f1d61b4702913943c536fc35a803a14f83a52
Thangnt <thangnt336@gmail.com>
2021-02-20 01:54:44 -0500

    Uploaded initial files for the portfolio project

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const mobileview = document.querySelector('.mobileview');
+    const nav = document.querySelector('.links');
+    const navLinks = document.querySelectorAll('.links li');
+
+    mobileview.addEventListener('click' , () => {
+   
+        // This is used to toggle the navigation bar in mobile view
+        nav.classList.toggle('nav-active');
+    
+        // This animates the links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `linkFade 0.5s ease forwards ${index / 7 + 1}s`;
+            }
+             console.log(index / 7);
+        });
+
+        // This animates the 3 bars
+        mobileview.classList.toggle('icon');
+
+    });
+}
+
+navSlide();
