8395c9a5a703dc6521173c6ae0833a6f7a12ab09
Thangnt <thangnt336@gmail.com>
2021-06-01 20:11:45 -0400

    updated portfolio with projects and editted styling

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,14 @@ const navSlide = () => {
 }
 
 navSlide();
+
+
+function openList1() {
+    var list = document.getElementById("ollist");
+
+    if (list.style.display == "none"){
+        list.style.display = "block";
+    }else{
+        list.style.display = "none";
+    }
+}
